c57bac2b39a4dfc1d2791858e1dadbd3c9116e46
Yosiya Hinosawa <stibium121@gmail.com>
2013-06-29 16:42:14 +0900

    Add junit and robolectric

--- src/test/java/org/kt3k/straw/test/StrawTest.java
+++ src/test/java/org/kt3k/straw/test/StrawTest.java
@@ -1,4 +1,17 @@
 package org.kt3k.straw.test;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
 public class StrawTest {
+
+    @Test
+    public void testMe() {
+        assertTrue(true);
+    }
 }
