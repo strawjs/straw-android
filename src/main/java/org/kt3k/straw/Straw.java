68c68b51d3d536941f3c20b4a753314201d879b2
Yosiya Hinosawa <stibium121@gmail.com>
2013-06-29 16:51:20 +0900

    Add test of hello

---
+++ src/main/java/org/kt3k/straw/Straw.java
@@ -0,0 +1,7 @@
+package org.kt3k.straw;
+
+public class Straw {
+    public static String hello() {
+        return "hello";
+    }
+}
--- src/test/java/org/kt3k/straw/test/StrawTest.java
+++ src/test/java/org/kt3k/straw/test/StrawTest.java
@@ -7,11 +7,18 @@ import static org.junit.Assert.*;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 
+import org.kt3k.straw.*;
+
 @RunWith(RobolectricTestRunner.class)
 public class StrawTest {
 
     @Test
     public void testMe() {
         assertTrue(true);
+    }
+
+    @Test
+    public void testHello() {
+        assertEquals("hello", Straw.hello());
     }
 }
