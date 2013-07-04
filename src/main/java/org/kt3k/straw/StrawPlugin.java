81e15aa7743d3eb247219d93f5e6326dc26d4177
Yosiya Hinosawa <stibium121@gmail.com>
2013-07-04 22:03:31 +0900

    Add org.codehaus.jackson

--- src/main/java/org/kt3k/straw/StrawPlugin.java
+++ src/main/java/org/kt3k/straw/StrawPlugin.java
@@ -3,6 +3,11 @@ package org.kt3k.straw;
 import android.webkit.WebView;
 import android.content.Context;
 
+import java.util.HashMap;
+import java.lang.reflect.Method;
+
+import org.codehaus.jackson.map.ObjectMapper;
+
 import org.json.JSONArray;
 
 abstract public class StrawPlugin {
@@ -10,6 +15,15 @@ abstract public class StrawPlugin {
     private WebView webView;
 
     private Context context;
+
+    private HashMap<String, Method> methodMap = new HashMap<String, Method>();
+
+    public StrawPlugin() {
+        Method[] methods = this.getClass().getMethods();
+        for (Method m: methods) {
+            this.methodMap.put(m.getName(), m);
+        }
+    }
 
     public void setWebView(WebView webView) {
         this.webView = webView;
@@ -25,16 +39,27 @@ abstract public class StrawPlugin {
 
     public JSONArray exec(String action, JSONArray array, String callbackId) {
         try {
-            this.getClass().getMethod(action, JSONArray.class).invoke(this, array);
+            Method targetMethod = this.methodMap.get(action);
+
+            Class parameterType = this.getFirstParameterClass(targetMethod);
+
+            //targetMethod.invoke(this, ObjectMapper.convertValue(parameterType))
         } catch (SecurityException e) {
             System.out.println("cannot execute action: " + action + ", arguments: " + array);
-        } catch (IllegalAccessException e) {
-            System.out.println("cannot execute action: " + action + ", arguments: " + array);
-        } catch (java.lang.reflect.InvocationTargetException e) {
-            System.out.println("cannot execute action: " + action + ", arguments: " + array);
-        } catch (NoSuchMethodException e) {
-            System.out.println("cannot execute action: " + action + ", arguments: " + array);
         }
+        return null;
+    }
+
+    private static Class getFirstParameterClass(Method m) {
+        Class<?>[] types = m.getParameterTypes();
+
+        if (types.length > 1) {
+            System.out.println("error too many parameter declared.");
+            return null;
+        } else if (types.length == 1) {
+            return types[0];
+        }
+
         return null;
     }
 }
