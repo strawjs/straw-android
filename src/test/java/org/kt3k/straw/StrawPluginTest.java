b0100e68aa65c027cf9537211eda5b419b70147c
Yosiya Hinosawa <stibium121@gmail.com>
2013-07-06 10:43:33 +0900

    Separate StrawPlugin test

--- src/test/java/org/kt3k/straw/StrawPluginTest.java
+++ src/test/java/org/kt3k/straw/StrawPluginTest.java
@@ -13,30 +13,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(RobolectricTestRunner.class)
-public class StrawPluginManagerTest {
+public class StrawPluginTest {
 
     @Test
-    public void testConstuctor() {
-        Activity activity = new Activity();
-        WebView webView = new WebView(activity);
-        StrawPluginManager pm = new StrawPluginManager(webView, activity);
-    }
-
-    @Test
-    public void testLoadPlugins() {
-        Activity activity = new Activity();
-        WebView webView = new WebView(activity);
-        StrawPluginManager pm = new StrawPluginManager(webView, activity);
-
-        pm.loadPluginByName("org.kt3k.straw.DummyStrawPlugin");
-
-        StrawPlugin dummyPlugin = pm.getPluginByName("dummy");
-        assertNotNull(dummyPlugin);
-        assertEquals("org.kt3k.straw.DummyStrawPlugin", dummyPlugin.getClass().getCanonicalName());
-    }
-
-    @Test
-    public void testPluginExecution() {
+    public void testPluginActionExecution() {
         Activity activity = new Activity();
         WebView webView = new WebView(activity);
         StrawPluginManager pm = new StrawPluginManager(webView, activity);
@@ -46,6 +26,10 @@ public class StrawPluginManagerTest {
         StrawPlugin dummyPlugin = pm.getPluginByName("dummy");
         assertEquals("{\"c\":\"foo\",\"d\":\"bar\"}", dummyPlugin.exec("dummyAction", "{\"a\":\"foo\",\"b\":\"bar\"}"));
         assertEquals("{\"c\":\"baz\",\"d\":\"baz\"}", dummyPlugin.exec("dummyAction", "{\"a\":\"baz\",\"b\":\"baz\"}"));
+
+        assertEquals("null", dummyPlugin.exec("dummyAction2", null));
+        assertEquals("{\"c\":null,\"d\":null}", dummyPlugin.exec("dummyAction3", null));
+        assertEquals("{\"c\":\"ddd\",\"d\":null}", dummyPlugin.exec("dummyAction4", null));
     }
 
 }
