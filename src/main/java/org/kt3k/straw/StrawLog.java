88907619427ec4b67718f174cd355e73e3fd2222
Yosiya Hinosawa <stibium121@gmail.com>
2013-07-15 18:01:25 +0900

    Make stack trace printing optional

--- src/main/java/org/kt3k/straw/StrawLog.java
+++ src/main/java/org/kt3k/straw/StrawLog.java
@@ -11,17 +11,34 @@ public class StrawLog {
 
 	static Printer output = new StrawErrorPrinter();
 
+	static Boolean printStackTrace = true;
+
 	private static void printError(String message, Exception e) {
 		output.println(message);
 
 		if (e != null) {
 			output.println(e.toString());
-			output.println(StrawUtil.join(e.getStackTrace(), "\n"));
+
+			if (StrawLog.printStackTrace) {
+				output.println(getStackTraceString(e));
+			}
 		}
 	}
 
 	public static void setPrinter(Printer printer) {
-		output = printer;
+		StrawLog.output = printer;
+	}
+
+	public static Boolean getPrintStackTrace() {
+		return StrawLog.printStackTrace;
+	}
+
+	public static void setPrintStackTrace(Boolean printStackTrace) {
+		StrawLog.printStackTrace = printStackTrace;
+	}
+
+	private static String getStackTraceString(Exception e) {
+		return StrawUtil.join(e.getStackTrace(), "\n");
 	}
 
 	public static void printFrameworkError(Exception e, String message) {
