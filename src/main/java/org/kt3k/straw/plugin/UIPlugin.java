59efdb7177d0718b2dd850728cd9a182533fbcb4
Yosiya Hinosawa <stibium121@gmail.com>
2013-07-18 20:32:25 +0900

    Add toast actions

--- src/main/java/org/kt3k/straw/plugin/UIPlugin.java
+++ src/main/java/org/kt3k/straw/plugin/UIPlugin.java
@@ -3,14 +3,34 @@ package org.kt3k.straw.plugin;
 import org.kt3k.straw.PluginAction;
 import org.kt3k.straw.StrawDrink;
 
+import android.widget.Toast;
+
 public class UIPlugin extends org.kt3k.straw.StrawPlugin {
 
 	@Override
 	public String getName() {
 		return "UI";
 	}
-	
+
 	@PluginAction
 	public void finish(Object _, StrawDrink drink) {
+		this.activity.finish();
 	}
+
+	static class ToastParam {
+		public String text;
+	}
+
+	@PluginAction
+	public void toast(ToastParam param, StrawDrink drink) {
+		Toast.makeText(activity, param.text, Toast.LENGTH_SHORT).show();
+	}
+
+	@PluginAction
+	public void toastLong(ToastParam param, StrawDrink drink) {
+		Toast.makeText(activity, param.text, Toast.LENGTH_LONG).show();
+	}
+
+
+
 }
