f1db6a2933e1efb013dc83c712c0e50e8f4e0ff1
Yosiya Hinosawa <stibium121@gmail.com>
2013-07-20 16:29:10 +0900

    Add placeholder for OptionsMenuPlugin

---
+++ src/main/java/org/kt3k/straw/plugin/OptionsMenuPlugin.java
@@ -0,0 +1,12 @@
+package org.kt3k.straw.plugin;
+
+import org.kt3k.straw.StrawPlugin;
+
+public class OptionsMenuPlugin extends StrawPlugin {
+
+	@Override
+	public String getName() {
+		return "optionsMenu";
+	}
+
+}
