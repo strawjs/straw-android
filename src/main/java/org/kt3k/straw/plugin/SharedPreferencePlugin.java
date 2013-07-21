ad3333a7269b72fef77959e7477f249d6674e5cd
Yosiya Hinosawa <stibium121@gmail.com>
2013-07-21 20:31:01 +0900

    Add placeholder SharedPreferencePlugin

---
+++ src/main/java/org/kt3k/straw/plugin/SharedPreferencePlugin.java
@@ -0,0 +1,12 @@
+package org.kt3k.straw.plugin;
+
+import org.kt3k.straw.StrawPlugin;
+
+public class SharedPreferencePlugin extends StrawPlugin {
+
+	@Override
+	public String getName() {
+		return "sharedPreference";
+	}
+
+}
