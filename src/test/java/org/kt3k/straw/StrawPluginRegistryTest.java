21a5afd177a72b50eacc226d3965149a9d9f4e47
Yosiya Hinosawa <stibium121@gmail.com>
2013-08-03 22:07:17 +0900

    Clean up test code

--- src/test/java/org/kt3k/straw/StrawPluginRegistryTest.java
+++ src/test/java/org/kt3k/straw/StrawPluginRegistryTest.java
@@ -4,9 +4,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 
-import android.webkit.WebView;
-import android.app.Activity;
+import static org.mockito.Mockito.*;
 
+import android.webkit.WebView;
 import org.robolectric.RobolectricTestRunner;
 
 @RunWith(RobolectricTestRunner.class)
@@ -14,59 +14,38 @@ public class StrawPluginRegistryTest {
 
     @Test
     public void testConstuctor() {
-        Activity activity = new Activity();
-        WebView webView = new WebView(activity);
-        new StrawPluginRegistry(webView);
+        new StrawPluginRegistry(mock(WebView.class));
     }
 
     @Test
     public void testLoadPluginsByName() {
-        Activity activity = new Activity();
-        WebView webView = new WebView(activity);
-        StrawPluginRegistry pm = new StrawPluginRegistry(webView);
+        StrawPluginRegistry registry = new StrawPluginRegistry(mock(WebView.class));
 
-        pm.loadPlugins(new String[]{"org.kt3k.straw.DummyStrawPlugin"});
+        registry.loadPlugins(new String[]{"org.kt3k.straw.DummyStrawPlugin"});
 
-        StrawPlugin dummyPlugin = pm.getPluginByName("dummy");
+        StrawPlugin dummyPlugin = registry.getPluginByName("dummy");
         assertNotNull(dummyPlugin);
         assertEquals("org.kt3k.straw.DummyStrawPlugin", dummyPlugin.getClass().getCanonicalName());
     }
 
     @Test
     public void testLoadPluginByName() {
-        Activity activity = new Activity();
-        WebView webView = new WebView(activity);
-        StrawPluginRegistry pm = new StrawPluginRegistry(webView);
+        StrawPluginRegistry registry = new StrawPluginRegistry(mock(WebView.class));
 
-        pm.loadPluginByName("org.kt3k.straw.DummyStrawPlugin");
+        registry.loadPluginByName("org.kt3k.straw.DummyStrawPlugin");
 
-        StrawPlugin dummyPlugin = pm.getPluginByName("dummy");
+        StrawPlugin dummyPlugin = registry.getPluginByName("dummy");
         assertNotNull(dummyPlugin);
         assertEquals("org.kt3k.straw.DummyStrawPlugin", dummyPlugin.getClass().getCanonicalName());
     }
 
-    /*@Test
-    public void testLoadPluginsByClass() {
-        Activity activity = new Activity();
-        WebView webView = new WebView(activity);
-        StrawPluginRegistry pm = new StrawPluginRegistry(webView);
-
-        pm.loadPlugins(new Class<? extends StrawPlugin>[]{org.kt3k.straw.DummyStrawPlugin.class});
-
-        StrawPlugin dummyPlugin = pm.getPluginByName("dummy");
-        assertNotNull(dummyPlugin);
-        assertEquals("org.kt3k.straw.DummyStrawPlugin", dummyPlugin.getClass().getCanonicalName());
-    }*/
-
     @Test
     public void testLoadPluginByClass() {
-        Activity activity = new Activity();
-        WebView webView = new WebView(activity);
-        StrawPluginRegistry pm = new StrawPluginRegistry(webView);
+        StrawPluginRegistry registry = new StrawPluginRegistry(mock(WebView.class));
 
-        pm.loadPluginByClass(org.kt3k.straw.DummyStrawPlugin.class);
+        registry.loadPluginByClass(org.kt3k.straw.DummyStrawPlugin.class);
 
-        StrawPlugin dummyPlugin = pm.getPluginByName("dummy");
+        StrawPlugin dummyPlugin = registry.getPluginByName("dummy");
         assertNotNull(dummyPlugin);
         assertEquals("org.kt3k.straw.DummyStrawPlugin", dummyPlugin.getClass().getCanonicalName());
     }
