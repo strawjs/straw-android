55e438c95f7bd1915efe9b744d649230aece9253
Yosiya Hinosawa <stibium121@gmail.com>
2013-08-09 00:30:55 +0900

    Add https support

--- src/main/java/org/kt3k/straw/plugin/HttpPlugin.java
+++ src/main/java/org/kt3k/straw/plugin/HttpPlugin.java
@@ -4,6 +4,15 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.X509TrustManager;
 
 import org.kt3k.straw.StrawDrink;
 import org.kt3k.straw.StrawPlugin;
@@ -14,6 +23,7 @@ public class HttpPlugin extends StrawPlugin {
 	static final String URL_MALFORMED_ERROR = "0";
 	static final String CANNOT_CONNECT_ERROR = "1";
 	static final String CANNOT_READ_ERROR = "2";
+	static final String SSL_UNAVAILABLE = "3";
 
 	@Override
 	public String getName() {
@@ -35,25 +45,40 @@ public class HttpPlugin extends StrawPlugin {
 
 	@PluginAction
 	public void get(HttpParam param, StrawDrink drink) {
-		URL url = null;
-		HttpURLConnection conn = null;
-		String content = null;
+		HttpURLConnection conn;
 
 		try {
-			url = new URL(param.url);
+			conn = createConnection(param.url, drink);
 		} catch (MalformedURLException e) {
 			drink.fail(URL_MALFORMED_ERROR, "URL format is wrong: " + param.url);
 
 			return;
-		}
-
-		try {
-			conn = (HttpURLConnection)url.openConnection();
 		} catch (IOException e) {
 			drink.fail(CANNOT_CONNECT_ERROR, "cannot connect to url: " + param.url);
 
 			return;
 		}
+
+		if (conn == null) {
+			return;
+		}
+
+		if (param.url.startsWith("https")) {
+			try {
+				setSSLContext(conn);
+
+			} catch (NoSuchAlgorithmException e) {
+				drink.fail(SSL_UNAVAILABLE, "SSL connection is unavailable: " + param.url);
+
+				return;
+			} catch (KeyManagementException e) {
+				drink.fail(SSL_UNAVAILABLE, "SSL connection is unavailable: " + param.url);
+
+				return;
+			}
+		}
+
+		String content = null;
 
 		try {
 			content = inputStreamToString(conn.getInputStream());
@@ -72,5 +97,25 @@ public class HttpPlugin extends StrawPlugin {
 
 	private static String inputStreamToString(java.io.InputStream stream) {
 		return new java.util.Scanner(stream).useDelimiter("\\A").next();
+	}
+
+	private static HttpURLConnection createConnection(String spec, StrawDrink drink) throws MalformedURLException, IOException {
+		URL url = new URL(spec);
+
+		HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+
+		return conn;
+	}
+
+	private static void setSSLContext(HttpURLConnection conn) throws NoSuchAlgorithmException, KeyManagementException {
+		SSLContext ctx = SSLContext.getInstance("TLS");
+
+		ctx.init(null, new X509TrustManager[]{new X509TrustManager(){
+			public void checkClientTrusted(X509Certificate[] chain,String authType) {}
+			public void checkServerTrusted(X509Certificate[] chain, String authType) {}
+			public X509Certificate[] getAcceptedIssuers() { return null; }
+		}}, null);
+
+		((HttpsURLConnection)conn).setSSLSocketFactory(ctx.getSocketFactory());
 	}
 }
