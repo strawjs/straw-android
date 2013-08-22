6c5512da9e1965c2c61225dc80b67c2a25f80d7e
Yosiya Hinosawa <stibium121@gmail.com>
2013-08-22 22:57:32 +0900

    Add annotations Background and RunOnUiThread

--- src/main/java/org/kt3k/straw/RunOnUiThread.java
+++ src/main/java/org/kt3k/straw/RunOnUiThread.java
@@ -1,11 +1,11 @@
 package org.kt3k.straw;
 
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Target;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface PluginAction {
+public @interface RunOnUiThread {
 }
