1965f7be4e5eb7f6ce890303f4ec415290ecdab0
Yosiya Hinosawa <stibium121@gmail.com>
2013-10-12 15:37:20 +0900

    Add comments

--- src/main/java/org/kt3k/straw/RunOnUiThread.java
+++ src/main/java/org/kt3k/straw/RunOnUiThread.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation for action methods to indicate that the method run on UiThread (main thread)
+ * This is useful for ui manipulation plugins
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RunOnUiThread {
