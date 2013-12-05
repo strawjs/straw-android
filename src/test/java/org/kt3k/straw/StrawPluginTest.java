e525f805f216a097cdd9bc42927b528523af78a6
Yoshiya Hinosawa <stibium121@gmail.com>
2013-12-05 13:31:58 +0900

    Remove obsolete comments

--- src/test/java/org/kt3k/straw/StrawPluginTest.java
+++ src/test/java/org/kt3k/straw/StrawPluginTest.java
@@ -30,55 +30,6 @@ public class StrawPluginTest {
 		mock(StrawDrink.class);
 	}
 
-	/*
-	@Test
-	public void testPluginActionExecution() throws InterruptedException {
-		when(this.mockDrink.getActionName()).thenReturn("dummyAction");
-		when(this.mockDrink.getArgumentJson()).thenReturn("{\"a\":\"foo\",\"b\":\"bar\"}");
-
-		this.dummyPlugin.exec(this.mockDrink);
-
-		DummyStrawPlugin.DummyActionResult res = new DummyStrawPlugin.DummyActionResult();
-		res.c = "foo";
-		res.d = "bar";
-		verify(this.mockDrink, timeout(1000)).success(res);
-
-
-		when(this.mockDrink.getArgumentJson()).thenReturn("{\"a\":\"baz\",\"b\":\"baz\"}");
-		this.dummyPlugin.exec(this.mockDrink);
-		res.c = "baz";
-		res.d = "baz";
-		verify(this.mockDrink, timeout(1000)).success(res);
-
-
-		when(this.mockDrink.getActionName()).thenReturn("dummyActionSuccessWithEmptyResult");
-		this.dummyPlugin.exec(this.mockDrink);
-		res.c = null;
-		res.d = null;
-		verify(this.mockDrink, timeout(1000)).success(res);
-	}
-
-	@Test
-	public void testActionWithoutAnnotation() throws InterruptedException {
-		when(this.mockDrink.getActionName()).thenReturn("dummyActionWithoutAnnotation");
-		when(this.mockDrink.getArgumentJson()).thenReturn("{}");
-
-		this.dummyPlugin.exec(this.mockDrink);
-
-		verify(this.mockDrink, timeout(1000).never()).success(any());
-	}
-
-	@Test
-	public void testActionWithWrongParameterTypes() throws InterruptedException {
-		when(this.mockDrink.getActionName()).thenReturn("dummyActionWithWrongParameterTypes");
-		when(this.mockDrink.getArgumentJson()).thenReturn("{}");
-
-		this.dummyPlugin.exec(this.mockDrink);
-
-		verify(this.mockDrink, timeout(1000).never()).success(any());
-	}
-	 */
-
 	@Test
 	public void testWebViewIsNotNull() {
 		assertNotNull(this.dummyPlugin.webView);
@@ -93,56 +44,6 @@ public class StrawPluginTest {
 	public void testActivityIsNotNull() {
 		assertNotNull(this.dummyPlugin.activity);
 	}
-
-	/*
-	@Test
-	public void testJsonParseErrorWhenExec() {
-		when(this.mockDrink.getActionName()).thenReturn("dummyAction");
-		when(this.mockDrink.getArgumentJson()).thenReturn("ABC");
-		when(this.mockDrink.toString()).thenReturn("baz");
-
-		Printer printer = mock(Printer.class);
-		StrawLog.setPrinter(printer);
-		StrawLog.setPrintStackTrace(false);
-
-		this.dummyPlugin.exec(this.mockDrink);
-
-		verify(printer, timeout(1000)).println("Straw Framework Error: JSON Parse Error: baz");
-	}
-
-	@Test
-	public void testErrorInsidePluginAction() {
-		when(this.mockDrink.getActionName()).thenReturn("throwError");
-		when(this.mockDrink.getArgumentJson()).thenReturn("{}");
-		when(this.mockDrink.toString()).thenReturn("baz");
-
-		Printer printer = mock(Printer.class);
-		StrawLog.setPrinter(printer);
-		StrawLog.setPrintStackTrace(false);
-
-		this.dummyPlugin.exec(this.mockDrink);
-
-		verify(printer, timeout(1000).times(2)).println("Straw Framework Error: cannot invoke action method (invocation target exception): baz");
-		verify(printer, timeout(1000)).println("java.lang.reflect.InvocationTargetException");
-		verify(printer, timeout(1000)).println("java.lang.ArithmeticException: / by zero");
-	}
-
-	@Test
-	public void testActionWithWrongAccessModifier() {
-		when(this.mockDrink.getActionName()).thenReturn("actionWithWrongAccessModifier");
-		when(this.mockDrink.getArgumentJson()).thenReturn("{}");
-		when(this.mockDrink.toString()).thenReturn("baz");
-
-		Printer printer = mock(Printer.class);
-		StrawLog.setPrinter(printer);
-		StrawLog.setPrintStackTrace(false);
-
-		this.dummyPlugin.exec(this.mockDrink);
-
-		verify(printer, timeout(1000)).println("Straw Framework Error: cannot invoke action method (illegal access exception): baz");
-		verify(printer, timeout(1000)).println("java.lang.IllegalAccessException: Class org.kt3k.straw.StrawPluginAction can not access a member of class org.kt3k.straw.DummyStrawPlugin with modifiers \"private\"");
-	}
-	 */
 
 	@Test
 	public void testSingleStringParam() {
