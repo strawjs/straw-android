9f12af67a1b85a454c24825475359c70a7bbc4d9
Yoshiya Hinosawa <stibium121@gmail.com>
2013-12-17 00:00:23 +0900

    Add StrawEventHandlerFactory tests

--- src/test/java/org/kt3k/straw/StrawEventHandlerFactoryTest.java
+++ src/test/java/org/kt3k/straw/StrawEventHandlerFactoryTest.java
@@ -1,5 +1,23 @@
 package org.kt3k.straw;
 
+import java.util.List;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class StrawEventHandlerFactoryTest {
+
+	@Test
+	public void testConstructor() {
+		assertNotNull(new StrawEventHandlerFactory());
+	}
+	
+	
+	@Test
+	public void testCreateWithPlugins() {
+		List<StrawEventHandler> handlers = StrawEventHandlerFactory.create(new StrawPlugin[]{new DummyStrawPlugin()});
+		
+		assertEquals(3, handlers.size());
+	}
 
 }
