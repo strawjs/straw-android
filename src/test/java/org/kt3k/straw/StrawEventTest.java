6f37617e9341de3bf632003bf5610efc3b2daf14
Yoshiya Hinosawa <stibium121@gmail.com>
2013-12-18 15:16:02 +0900

    Add a StrawEvent.Type test

---
+++ src/test/java/org/kt3k/straw/StrawEventTest.java
@@ -0,0 +1,14 @@
+package org.kt3k.straw;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class StrawEventTest {
+	
+	// only for coverage report
+	@Test
+	public void testTypeConstructor() {
+		assertNotNull(new StrawEvent.Type());
+	}
+
+}
